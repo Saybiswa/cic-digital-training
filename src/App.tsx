2d4aec3dfdc709e5efff8f1e3d3c67c63a233c24
Sayani Biswas <sayanibiswas.16@nshm.edu.in>
2026-03-13 15:28:18 +0530

    fix Login filename

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import AdminLogin from "./pages/AdminLogin";
